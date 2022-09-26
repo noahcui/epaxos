0f63eed4e49248a3b0630b5caee5e03b6ce92110
Noah Tsui <noahcui@hotmail.com>
2022-09-26 17:16:23 -0400

    u

--- src/mencius/mencius.go
+++ src/mencius/mencius.go
@@ -15,7 +15,7 @@ import (
 
 const CHAN_BUFFER_SIZE = 200000
 const WAIT_BEFORE_SKIP_MS = 50
-const NB_INST_TO_SKIP = 100000
+const NB_INST_TO_SKIP = 100
 const MAX_SKIPS_WAITING = 20
 const TRUE = uint8(1)
 const FALSE = uint8(0)
