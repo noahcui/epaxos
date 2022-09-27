984276823afe18396377bc9414aaad54db0c8df3
Noah Tsui <noahcui@hotmail.com>
2022-09-27 10:35:41 -0400

    upload

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ go build server
 go build client
 go build master
 
-scp -r benchmarker:epaxos/bin server1:epaxos/
-scp -r benchmarker:epaxos/bin server2:epaxos/
-scp -r benchmarker:epaxos/bin server3:epaxos/
