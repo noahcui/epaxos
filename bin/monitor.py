dde9f65de15704a454a60f190cd7aa980af5662d
noahcui <38910047+noahcui@users.noreply.github.com>
2022-12-29 02:49:12 -0500

    uploading

---
+++ bin/monitor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import psutil
+import time
+
+for i in range(100):
+    time.sleep(1)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().used
+    print("%s,%s" % (cpu, mem/(1024*1024)))
